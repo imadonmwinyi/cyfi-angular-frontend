e33a2c89dd0d560fdf784c9352a04ff09b489f4f
osazee <osasfrank246@gmail.com>
2025-04-27 06:53:05 +0600

    made change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { RegistrationComponent } from './component/registration/registration.component';
+import { DohComponent } from './component/doh/doh.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-    { path: 'register', component: RegistrationComponent } // Route to form page
+    { path: 'register', component: RegistrationComponent }, // Route to form page
+    {path: 'doh', component: DohComponent}
   
 ];
 
